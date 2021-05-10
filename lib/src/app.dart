482fec7b27b2fb85c4f7f5a829a36becf7cbb9a6
Ibad Siddiqui <ibadsiddiqui.u.siddiqui@gsk.com>
2021-05-10 10:37:37 +0500

    themes and icon updated

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:quran_marker/src/pages/dashboard.dart';
+import 'package:quran_marker/src/pages/theme.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: getTheme(),
+      home: MyHomePage(title: ''),
+    );
+  }
+}
---
+++ lib/src/pages/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+ThemeData getTheme() {
+  return ThemeData(
+    appBarTheme: AppBarTheme(backgroundColor: Color.fromRGBO(47, 82, 50, 1)),
+    // primaryColor:  Color.fromRGBO(47, 82, 50, 0),
+
+    // scaffoldBackgroundColor: Colors.white,
+    // primaryColor: Color(0xFF281700),
+    // focusColor: Color(0xFF89D1F0),
+    // primarySwatch: Colors.blue,
+    // disabledColor: Color.fromRGBO(120, 132, 158, 1),
+    // textTheme: TextTheme(
+    //   button: TextStyle(
+    //     fontWeight: FontWeight.bold,
+    //     color: Color(0xFF89D1F0),
+    //   ),
+    //   headline1: TextStyle(
+    //     color: Color(0xFF281700),
+    //     fontSize: 44,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    //   headline3: TextStyle(
+    //     color: Color(0xFF281700),
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    //   headline4: TextStyle(
+    //     color: Color(0xFF281700),
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    //   headline5: TextStyle(
+    //     color: Color(0xFF281700),
+    //     fontSize: 22,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    //   headline6: TextStyle(
+    //     color: Color(0xFF281700),
+    //     fontSize: 14,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    // ),
+    // visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
